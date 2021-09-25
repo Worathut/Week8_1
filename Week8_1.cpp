caeba675430c32402720e6593ca83da8930368fe
WORATHUT JITTICHAI <64010759@KMITL.AC.TH>
2021-09-25 17:53:34 +0700

    Add project files.

---
+++ Week8_1.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int IsPrime(int);
+int main() {
+	int num;
+	scanf("%d", &num);
+	if (IsPrime(num) == 1) {
+		printf("%d is a prime number",num);
+	}
+	else {
+		printf("%d is not a prime number",num);
+	}
+	return 0;
+}
+int IsPrime(int n) {
+	if (n == 0 || n == 1) {
+		return 0;
+	}
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
